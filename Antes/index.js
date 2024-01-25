258cc9e60fbb653db51589515ed2704c17532e83
soaresgustavon <gu.soares0608@gmail.com>
2024-01-25 16:53:36 -0300

    criando a função de armazenamento do número escolhido pelo usuário

--- Antes/index.js
+++ Antes/index.js
@@ -1,4 +1,13 @@
+let computerNumber
+let userNumbers = []
+
 function init(){
     computerNumber = Math.floor(Math.random() * 101)
     console.log(computerNumber)
+}
+
+function compareNumber(){
+    const userNumber = Number(document.getElementById('inputBox').value)
+    userNumbers.push(' ' + userNumber)
+    document.getElementById('guesses').innerHTML = userNumbers
 }
