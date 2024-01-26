07c9476c07bc5fb4dbadb2eb3e72e0f682aa41b2
soaresgustavon <gu.soares0608@gmail.com>
2024-01-26 12:10:37 -0300

    adicionando um número máximo de tentativas do usuário

--- Antes/index.js
+++ Antes/index.js
@@ -1,6 +1,7 @@
 let computerNumber
 let userNumbers = []
 let attempts = 0
+let maxGuesses = 10
 
 function init(){
     computerNumber = Math.floor(Math.random() * 101)
@@ -12,23 +13,28 @@ function compareNumber(){
     userNumbers.push(' ' + userNumber)
     document.getElementById('guesses').innerHTML = userNumbers
 
-if (userNumber > computerNumber){
-    document.getElementById('textOutput').innerHTML = 'Your number is to high'
-    document.getElementById('inputBox').value = ''
-    attempts++
-    document.getElementById('attempts').innerHTML = attempts
+    if(attempts < maxGuesses){
+        if (userNumber > computerNumber){
+            document.getElementById('textOutput').innerHTML = 'Your number is to high'
+            document.getElementById('inputBox').value = ''
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
+            }
+        
+        else if (userNumber < computerNumber){
+            document.getElementById('textOutput').innerHTML = 'Your number is to low'
+            document.getElementById('inputBox').value = ''
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
+            }
+        
+        else{
+            document.getElementById('textOutput').innerHTML = 'Congratulations!!!'
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
+            }
     }
-
-else if (userNumber < computerNumber){
-    document.getElementById('textOutput').innerHTML = 'Your number is to low'
-    document.getElementById('inputBox').value = ''
-    attempts++
-    document.getElementById('attempts').innerHTML = attempts
-    }
-
-else{
-    document.getElementById('textOutput').innerHTML = 'Congratulations!!!'
-    attempts++
-    document.getElementById('attempts').innerHTML = attempts
+    else{
+        document.getElementById('textOutput').innerHTML = 'You lose! The number of the computer was ' + computerNumber
     }
 }
