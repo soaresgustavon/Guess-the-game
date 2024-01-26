1580806d72a822ed4a3f5b703208ff9959375ae6
soaresgustavon <gu.soares0608@gmail.com>
2024-01-26 11:45:42 -0300

    adicionando o número de tentativas do usuário

--- Antes/index.js
+++ Antes/index.js
@@ -1,5 +1,6 @@
 let computerNumber
 let userNumbers = []
+let attempts = 0
 
 function init(){
     computerNumber = Math.floor(Math.random() * 101)
@@ -14,12 +15,20 @@ function compareNumber(){
 if (userNumber > computerNumber){
     document.getElementById('textOutput').innerHTML = 'Your number is to high'
     document.getElementById('inputBox').value = ''
+    attempts++
+    document.getElementById('attempts').innerHTML = attempts
     }
+
 else if (userNumber < computerNumber){
     document.getElementById('textOutput').innerHTML = 'Your number is to low'
     document.getElementById('inputBox').value = ''
+    attempts++
+    document.getElementById('attempts').innerHTML = attempts
     }
+
 else{
     document.getElementById('textOutput').innerHTML = 'Congratulations!!!'
+    attempts++
+    document.getElementById('attempts').innerHTML = attempts
     }
-} 
+}
