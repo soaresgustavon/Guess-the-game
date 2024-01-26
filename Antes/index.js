ffd6455f21f0d6bad3294eeb9cba224979c588f5
soaresgustavon <gu.soares0608@gmail.com>
2024-01-26 12:48:57 -0300

    finalizando o projeto com os detalhes finais

--- Antes/index.js
+++ Antes/index.js
@@ -1,13 +1,21 @@
+//Variáveis
 let computerNumber
 let userNumbers = []
 let attempts = 0
 let maxGuesses = 10
 
+//Função para gerar um número randômico
 function init(){
     computerNumber = Math.floor(Math.random() * 101)
     //console.log(computerNumber)
 }
 
+//Função para recarregar a página e reiniciar o jogo quando o usuário ganha ou perde
+function newGame(){
+    window.location.reload()
+}
+
+//Função para comparar o número escolhido pelo usuário com o escolhido pelo computaador
 function compareNumber(){
     const userNumber = Number(document.getElementById('inputBox').value)
     userNumbers.push(' ' + userNumber)
@@ -32,9 +40,15 @@ function compareNumber(){
             document.getElementById('textOutput').innerHTML = 'Congratulations!!!'
             attempts++
             document.getElementById('attempts').innerHTML = attempts
+
+            //Atributo para deixar o campo de digitação bloqueado quando o jogo acaba
+            document.getElementById('inputBox').setAttribute('Readonly', true)
             }
     }
     else{
         document.getElementById('textOutput').innerHTML = 'You lose! The number of the computer was ' + computerNumber
+        
+        //Atributo para deixar o campo de digitação bloqueado quando o jogo acaba
+        document.getElementById('inputBox').setAttribute('Readonly', true)
     }
 }
