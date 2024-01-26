4875fc11a58de424d11c8088546bcf8d19cbffc9
soaresgustavon <gu.soares0608@gmail.com>
2024-01-26 11:26:19 -0300

    adicionando if & else

--- Antes/index.js
+++ Antes/index.js
@@ -3,11 +3,23 @@ let userNumbers = []
 
 function init(){
     computerNumber = Math.floor(Math.random() * 101)
-    console.log(computerNumber)
+    //console.log(computerNumber)
 }
 
 function compareNumber(){
     const userNumber = Number(document.getElementById('inputBox').value)
     userNumbers.push(' ' + userNumber)
     document.getElementById('guesses').innerHTML = userNumbers
-}
+
+if (userNumber > computerNumber){
+    document.getElementById('textOutput').innerHTML = 'Your number is to high'
+    document.getElementById('inputBox').value = ''
+    }
+else if (userNumber < computerNumber){
+    document.getElementById('textOutput').innerHTML = 'Your number is to low'
+    document.getElementById('inputBox').value = ''
+    }
+else{
+    document.getElementById('textOutput').innerHTML = 'Congratulations!!!'
+    }
+} 
